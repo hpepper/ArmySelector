faac6faff3b7d57018d3d60a3e3e81b4469c3b70
H P <h.pepper@spam.no>
2014-07-22 00:55:25 +0200

    Building works for Windows and Android, though android app exits, probably due to the missing XML file.#

--- Builds/Android/jni/Android.mk
+++ Builds/Android/jni/Android.mk
@@ -8,10 +8,12 @@ include $(CLEAR_VARS)
 LOCAL_ARM_MODE := arm
 LOCAL_MODULE := juce_jni
 LOCAL_SRC_FILES := \
+  ../../../Source/Storage.cpp\
   ../../../Source/Model.cpp\
   ../../../Source/Controller.cpp\
   ../../../Source/MainComponent.cpp\
   ../../../Source/Main.cpp\
+  ../../../JuceLibraryCode/BinaryData.cpp\
   ../../../JuceLibraryCode/modules/juce_audio_basics/juce_audio_basics.cpp\
   ../../../JuceLibraryCode/modules/juce_audio_devices/juce_audio_devices.cpp\
   ../../../JuceLibraryCode/modules/juce_audio_formats/juce_audio_formats.cpp\
