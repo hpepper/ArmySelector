6e991bb4714f1acccfd63af23b9e59ccdba950c1
H P <h.pepper@spam.no>
2014-07-22 10:58:26 +0200

    Add the storage files, empty for now.

---
+++ Source/Storage.h
@@ -0,0 +1,23 @@
+/*
+  ==============================================================================
+
+    Storage.h
+    Created: 13 Jul 2014 12:01:39pm
+    Author:  henryp
+
+  ==============================================================================
+*/
+
+#ifndef STORAGE_H_INCLUDED
+#define STORAGE_H_INCLUDED
+
+// TODO V later support more than XML storage, but handle that in sub classes.
+
+class Storage {
+};
+
+
+
+
+
+#endif  // STORAGE_H_INCLUDED
